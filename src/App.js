ad4ed4ab3c15a9a894fc72adc1b925213bad8456
michael-mbugua <kamirimichael369@gmail.com>
2023-02-06 02:23:35 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import About from './components/About';
+import Education from './components/Education';
 import Home from './components/Home';
 import Skill from './components/Skill';
-
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-        <Home/>
-        <About/>
-        <Skill/>
+       <BrowserRouter>
+         <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/myskills' element={<Skill/>}/>
+            <Route path='/myEducation' element={<Education/>}/>
+         </Routes>
+       </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Education.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Education.css'
+
+function Education() {
+  return (
+    <div className='Education-header'>
+        <h2>Education</h2>
+      <div className="table-set">
+      <table>
+            <th>Year</th>
+            <th>School</th>
+            <th>Course</th>
+            <tr>
+                <td>2019 - present</td>
+                <td>Cooperative University of Kenya</td>
+                <td>Business Information and Technology</td>
+            </tr>
+            <tr>
+                <td>2014-2018</td>
+                <td>lorem 20</td>
+                <td>KCSE - high school education</td>
+            </tr>
+            
+            
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Education
